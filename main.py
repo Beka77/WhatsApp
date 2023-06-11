11a5f67189c2d0750f86cbdc8a4bc5ed7668797c
Beka77 <bekakasymjanov@gmail.com>
2023-06-11 20:29:49 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import pywhatkit
+#  send message to whatsapp user
+pywhatkit.sendwhatmsg_instantly(phone_no="{phone number}",message= "{massage}",)
+# send message to whatsapp group
+pywhatkit.sendwhatmsg_to_group_instantly("{name group}", "{massage}")
